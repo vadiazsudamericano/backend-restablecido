139a76538e25dda065ec936c3c1c12c23d859264
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-07-18 16:55:26 -0500

    fix: incluir ormconfig en build8

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,6 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
 export class CreateUserDto {
-  @IsNotEmpty()
   nombre: string;
-
-  @IsNotEmpty()
   apellido: string;
-
-  @IsEmail()
   email: string;
-
-  @IsNotEmpty()
   password: string;
 }
