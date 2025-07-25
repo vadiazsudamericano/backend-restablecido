357fcf1f60898d6c5115071589fd03fcdcca3f77
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-07-25 11:53:47 -0500

    Cambio crypto

---
+++ crypto-polyfill.js
@@ -0,0 +1,13 @@
+// crypto-polyfill.js
+if (typeof globalThis.crypto === 'undefined') {
+  const crypto = require('crypto');
+  globalThis.crypto = {
+    randomUUID: crypto.randomUUID || (() => {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0;
+        const v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    })
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 // RUTA: src/main.ts
-
+import './crypto-polyfill';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
+// ... resto de tus importaciones
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
