bcb3acc2139c4b8ee52822445ecab878467c1739
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-07-27 23:28:50 -0500

    Cambios port3

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   });
 
   await app.listen(process.env.PORT || 3000);
-  console.log(`🚀 Backend escuchando en el puerto ${process.env.PORT || 3000}`);
+  console.log(`🚀 Backend escuchando en el puerto ${process.env.PORT}`);
 }
 bootstrap();
