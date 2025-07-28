f6bbecd203e06f9565aadf4cdb7363f49e44e16c
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-07-27 23:17:29 -0500

    Cambios port4

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [],
-    credentials: true,
-  }); 
+  app.enableCors(); 
   const port = process.env.PORT || 3000;
   await app.listen(8080);
   console.log(`🚀 Backend escuchando en el puerto ${port}`);
