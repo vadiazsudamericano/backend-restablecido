0d0f603d8f9c8a9b6a8602d704a428808162d60d
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-07-28 02:30:18 -0500

    Cambios getnom

--- src/herramienta/herramienta.controller.ts
+++ src/herramienta/herramienta.controller.ts
@@ -1,17 +1,31 @@
-// src/herramienta/herramienta.controller.ts
-import { Controller, Get, Param, NotFoundException, ParseIntPipe } from '@nestjs/common';
-import { HerramientaService } from './herramienta.service';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Herramienta } from './herramienta.entity';
 
 @Controller('herramientas')
 export class HerramientaController {
-  constructor(private readonly herramientaService: HerramientaService) {}
+  constructor(
+    @InjectRepository(Herramienta)
+    private readonly herramientaRepository: Repository<Herramienta>,
+  ) {}
 
-  @Get(':id')
-  async findOne(@Param('id', ParseIntPipe) id: number) {
-    const herramienta = await this.herramientaService.findById(id);
+  @Get()
+  async getAll() {
+    return this.herramientaRepository.find();
+  }
+
+  @Get('nombre/:nombre')
+  async getPorNombre(@Param('nombre') nombre: string) {
+    const herramienta = await this.herramientaRepository.findOne({ where: { nombre } });
     if (!herramienta) {
-      throw new NotFoundException(`Herramienta con ID ${id} no encontrada`);
+      throw new NotFoundException('Herramienta no encontrada');
     }
     return herramienta;
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: number) {
+    return this.herramientaRepository.findOne({ where: { id } });
   }
 }
