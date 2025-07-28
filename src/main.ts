f3cde2ea559d1bbc6ce93b1897fe2d989f9087cb
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-07-28 00:19:04 -0500

    Cambios railway port

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors'; // ← CORRECTO
-
 import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilita CORS directamente
   app.enableCors({
-    origin: '*', // O tu frontend real, por ejemplo: 'https://tu-app.vercel.app'
+    origin: '*', // O especifica tu frontend: 'https://tu-app.vercel.app'
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
   const port = process.env.PORT || 8080;
-  await app.listen(port);
+  await app.listen(8080);
   console.log(`🚀 Backend escuchando en el puerto ${port}`);
 }
 bootstrap();
