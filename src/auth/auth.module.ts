9edba26a6c7a4384359d75e41cea0280d9eefc75
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-07-30 19:07:36 -0500

    cambios historial6

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,28 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from '../users/users.module';  // Asegúrate de que la ruta sea correcta
+import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    forwardRef(() => UsersModule),  // Utilizamos forwardRef aquí
+    forwardRef(() => UsersModule),
     PassportModule,
-    JwtModule.register({ secret: 'secretKey', signOptions: { expiresIn: '1h' } }),
+    ConfigModule, // 🔑 Importamos ConfigModule
+    JwtModule.registerAsync({
+      imports: [ConfigModule], // ✅ Necesario para leer .env
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'), // ✅ lee el secreto real
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
   ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
-  exports: [AuthService, JwtModule],  // Exportamos AuthService si lo necesitamos en otros módulos
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,7 +14,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || 'secretKey', // Asegúrate de tener esto en el .env
+      secretOrKey: configService.get<string>('JWT_SECRET')
+
     });
   }
 
